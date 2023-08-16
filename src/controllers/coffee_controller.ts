7c6179be972d7bead6e6979a358cc910a09f929c
nandinip92 <nandini.p8892@gmail.com>
2023-08-16 08:19:07 +0100

    feat: Implement GET endpoint to serve JSON data back with contoller and service to seperate business logic

---
+++ src/controllers/coffee_controller.ts
@@ -0,0 +1,12 @@
+//import Express from "express";
+import { Request, Response } from "express";
+// ❗ altered import
+import * as coffeeService from "../services/coffee_service";
+export const getCoffee = async (
+  req: Request<object, object, object, { coffeeName: string | undefined }>,
+  res: Response
+) => {
+  const coffeeName = req.query.coffeeName;
+  const coffee = coffeeService.getCoffee(coffeeName);
+  res.json(coffee).status(200);
+};
