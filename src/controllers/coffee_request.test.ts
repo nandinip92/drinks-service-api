8c42c7ce37a0d5838116f8cc64135067fa312c07
nandinip92 <nandini.p8892@gmail.com>
2023-08-16 08:46:46 +0100

    feat: adding more test cases

--- src/controllers/coffee_request.test.ts
+++ src/controllers/coffee_request.test.ts
@@ -11,5 +11,25 @@ describe(" Test Coffeelover endpoint request", () => {
       drinkType: "Coffee",
       name: "Latte",
     });
+
+    const res2 = await request(app)
+      .get("/coffee")
+      .query({ coffeeName: "Mocha" });
+
+    expect(res2.statusCode).toEqual(200);
+    expect(res2.body).toEqual({
+      drinkType: "Coffee",
+      name: "Mocha",
+    });
+  });
+
+  test("GET /coffee should return the name as Latte if coffeeName is provided ", async () => {
+    const res = await request(app).get("/coffee");
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toEqual({
+      drinkType: "Coffee",
+      name: "Latte",
+    });
   });
 });
